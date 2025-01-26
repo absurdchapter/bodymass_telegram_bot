7f281744e336591289ea96052cba1a77c7f88ed4
killermantle <>
2025-01-26 22:54:20 +0100

    db operations (with failsafes)

--- src/conversationdata.py
+++ src/conversationdata.py
@@ -1,7 +1,16 @@
+import sqlite3
+
 import aiosqlite
 
 sqlite_db_path = 'data/bodymass.sqlite'
 sqlite_db_users_conversation = 'users_conversation'
+sqlite_db_users_language = 'users_language'
+
+
+def _assert_enum_consistency(cls: type):
+    for k, v in vars(cls).items():
+        if not k.startswith('_'):
+            assert k == v
 
 
 class ConversationState:
@@ -12,29 +21,81 @@ class ConversationState:
     awaiting_language = 'awaiting_language'
 
 
+_assert_enum_consistency(ConversationState)
 conversation_states = [k for k in vars(ConversationState).keys() if not k.startswith('_')]
 
-for k, v in vars(ConversationState).items():
-    if not k.startswith('_'):
-        assert k == v
+
+class Language:
+    english = 'english'
+    russian = 'russian'
+
+
+default_language = Language.english
+
+_assert_enum_consistency(Language)
+languages = [k for k in vars(Language).keys() if not k.startswith('_')]
 
 
 async def get_conversation_data(user_id: int) -> dict:
     async with aiosqlite.connect(sqlite_db_path) as db:
+        result = dict()
+        result['conversation_state'] = await get_conversation_state(db, user_id)
+        result['language'] = await get_language(db, user_id) or default_language
+
+        return result
+
+
+async def get_conversation_state(db: aiosqlite.Connection, user_id: int) -> str:
+    async with db.cursor() as cursor:
+        query = f"SELECT conversation_state FROM {sqlite_db_users_conversation} " \
+                f"WHERE user_id = '{user_id}';"
+        await cursor.execute(query)
+        conversation_state = await cursor.fetchone()
+        conversation_state = conversation_state[0] if conversation_state is not None else 'init'
+        assert conversation_state in conversation_states
+
+        return conversation_state
+
+
+async def get_language(db: aiosqlite.Connection, user_id: int) -> str | None:
+    try:
         async with db.cursor() as cursor:
-            query = f"SELECT conversation_state FROM {sqlite_db_users_conversation} " \
+            query = f"SELECT language FROM {sqlite_db_users_language} " \
                     f"WHERE user_id = '{user_id}';"
             await cursor.execute(query)
-            conversation_state = await cursor.fetchone()
-            conversation_state = conversation_state[0] if conversation_state is not None else 'init'
-            assert conversation_state in conversation_states
-            return {'conversation_state': conversation_state}
+            language = await cursor.fetchone()
+            if language is not None:
+                language = language[0]
+                assert language in languages
+
+            return language
+    except sqlite3.OperationalError as exc:
+        if str(exc) == f'no such table: {sqlite_db_users_language}':
+            return
+        raise exc
 
 
 async def write_conversation_data(user_id: int, conversation_data: dict) -> None:
     async with aiosqlite.connect(sqlite_db_path) as db:
-        query = f"INSERT INTO {sqlite_db_users_conversation} (user_id, conversation_state) " \
-                f"VALUES ('{user_id}', '{conversation_data['conversation_state']}'); "
+        await write_conversation_state(db, conversation_data['conversation_state'], user_id)
+        if 'language' in conversation_data:
+            await write_language(db, conversation_data['language'], user_id)
 
+
+async def write_conversation_state(db: aiosqlite.Connection, conversation_state: str, user_id: int):
+    query = f"INSERT INTO {sqlite_db_users_conversation} (user_id, conversation_state) " \
+            f"VALUES ('{user_id}', '{conversation_state}'); "
+    await db.execute(query)
+    await db.commit()
+
+
+async def write_language(db: aiosqlite.Connection, language: str, user_id: int):
+    try:
+        query = f"INSERT INTO {sqlite_db_users_language} (user_id, language) " \
+                f"VALUES ('{user_id}', '{language}'); "
         await db.execute(query)
         await db.commit()
+    except sqlite3.OperationalError as exc:
+        if str(exc) == f'no such table: {sqlite_db_users_language}':
+            return
+        raise exc
